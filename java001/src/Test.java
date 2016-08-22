3c22d839389d5e7c0b8a78584bc7fb6ab8b491f0
whinewinni <forhwtkj@gmail.com>
2016-08-22 22:59:32 +0900

    testing2

--- java001/src/Test.java
+++ java001/src/Test.java
@@ -4,4 +4,11 @@
 public class Test {
     //흥해라 흥
     //박 띨띨아 나오나 보거랑 냥냥냥
+    public static void main(String[] args) {
+        for (int i= 2  ; i<= 9 ; i++){
+            for (int j=2 ; j<= 9 ; j++){
+                System.out.println(i +" X "+j+" = "+ (i*j));
+            }
+        }
+    }
 }
