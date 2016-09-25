4b81f2010eff81745513509b6749603656e5bfb1
whinewinni <forhwtkj@gmail.com>
2016-09-26 00:38:26 +0900

    add Delete controller

---
+++ calendar/src/main/java/or/whine/mvc/controller/Dog.java
@@ -0,0 +1,12 @@
+package or.whine.mvc.controller;
+
+public class Dog {
+    public static void main(String[] args) {
+        Dog chihuahua, welsh_corgi;
+
+        chihuahua = new Dog();
+    }
+}
+
+
+
--- calendar/src/main/java/or/whine/mvc/controller/TestClass.java
+++ calendar/src/main/java/or/whine/mvc/controller/TestClass.java
@@ -18,4 +18,5 @@ public class TestClass {
         tc.b();
         System.out.println(tc.a);
     }
+
 }
